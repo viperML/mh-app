8ed62f53367e2f5ed33a2b5df26c0219ed4d774d
Fernando Ayats <ayatsfer@gmail.com>
2025-04-12 17:36:22 +0200

    Break up code

---
+++ src/components/ArmorCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { ArmorPiece } from "../scripts/api";
+
+const props = defineProps<{
+    armor?: ArmorPiece | undefined;
+}>();
+</script>
+
+<template>
+    <div class="bg-zinc-800 p-7 rounded-md shadow-md">
+        <span class="m-2">
+            {{ props.armor?.name ?? "Not set" }}
+        </span>
+        <div class="text-xs m-2 flex flex-row gap-4">
+            <span v-for="skill of props.armor?.skills" v-bind:key="String(skill.id)">{{ skill.name }}</span>
+        </div>
+    </div>
+</template>
---
+++ src/components/ArmorSelect.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { reactive, ref, useTemplateRef, watchEffect } from "vue";
+import { armorKinds, type ArmorKind, type ArmorPiece } from "../scripts/api";
+import type { Skill } from "../scripts/skill";
+import ArmorCard from "./ArmorCard.vue";
+import { assert } from "tsafe/assert";
+import { parseInt2 } from "../scripts/util";
+
+const props = defineProps<{
+    allSkills: Map<number, Skill>;
+    allArmors: Map<number, ArmorPiece>;
+}>();
+
+const emit = defineEmits<{
+    change: [];
+    update: [value: string]; // named tuple syntax
+}>();
+
+const selected = ref("");
+watchEffect(() => {
+    emit("update", selected.value);
+});
+
+const armorDialog = useTemplateRef("armorDialog");
+
+const someArmor = props.allArmors.entries().next().value?.[1];
+assert(someArmor);
+
+const selectedArmor = reactive(
+    Object.fromEntries(
+        armorKinds.map(kind => {
+            const storedId = parseInt2(localStorage.getItem(kind) ?? "");
+            const res = storedId ? props.allArmors.get(storedId) : undefined;
+            console.log("Stored", kind, storedId, res);
+            return [kind, res];
+        }),
+    ),
+) as Record<ArmorKind, ArmorPiece | undefined>;
+
+for (const kind of armorKinds) {
+    watchEffect(() => {
+        localStorage.setItem(kind, String(selectedArmor[kind]?.id ?? ""));
+    });
+}
+
+const activeDialog = ref(armorKinds[0]);
+</script>
+
+<template>
+    <article class="card">
+        <template v-for="kind of armorKinds" v-bind:key="kind">
+            <button
+                @click="
+                    () => {
+                        activeDialog = kind;
+                        armorDialog?.showModal();
+                    }
+                "
+            >
+                <ArmorCard :armor="selectedArmor[kind]" />
+            </button>
+        </template>
+    </article>
+
+    <button class="bg-slate-600 p-2" @click="armorDialog?.showModal()">Show dialog</button>
+
+    <dialog ref="armorDialog" closedby="any">
+        <button class="bg-slate-600 p-2" @click="armorDialog?.close()">Close</button>
+        <template v-for="[id, armor] of allArmors" v-bind:key="String(id)">
+            <button
+                v-if="activeDialog === armor.kind"
+                @click="
+                    () => {
+                        selectedArmor[armor.kind] = armor;
+                        armorDialog?.close();
+                    }
+                "
+            >
+                <ArmorCard :armor="armor" />
+            </button>
+        </template>
+    </dialog>
+</template>
+
+<style scoped>
+.card {
+    display: grid;
+    grid-template-columns: auto;
+    gap: 64px;
+    margin: 64px;
+}
+
+dialog {
+    /* display: grid; */
+    /* width: 100dvw; */
+    justify-content: center;
+    background-color: rgb(47, 47, 47);
+    grid-template-columns: 200px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: 0;
+    width: 300px;
+}
+
+dialog[open] {
+    display: grid;
+    justify-content: center;
+    gap: 10px;
+}
+</style>
+
+<style>
+body:has(dialog[open]) {
+    overflow: hidden;
+}
+</style>
